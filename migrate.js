d1de2483d20fb9a04b458ae95b2b8a63d1c3d5c3
germanherculesgh <germanhercules@gmail.com>
2026-06-19 15:39:50 -0700

    Delete migrate.js

--- migrate.js
+++
@@ -1,8 +0,0 @@
-const {runMigrations} = require('./migrate-lib');
-
-runMigrations({
-  databaseUrl: process.env.DATABASE_URL || '',
-}).catch(err => {
-  console.error(err.stack || String(err));
-  process.exit(1);
-});
